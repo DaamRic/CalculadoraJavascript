b06ecbcb225264d7419892395ca44e0c9db1d1b7
Damian Ricardi <damianricardi96@gmail.com>
2023-06-19 11:07:13 -0300

    agregamos map para mostrar todos los resultados de las operaciones realizadas

--- js/index.js
+++ js/index.js
@@ -1,56 +1,62 @@
 //Calculadora
 
-//Funcion para calcular
-function calcular(operacion, primerNumero, segundoNumero) {
-    let resultado = 0;
-    
-    switch (operacion) {
-      case '+':
-        resultado = primerNumero + segundoNumero;
-        break;
-      case '-':
-        resultado = primerNumero - segundoNumero;
-        break;
-      case '*':
-        resultado = primerNumero * segundoNumero;
-        break;
-      case '/':
-        resultado = primerNumero / segundoNumero;
-        break;
-      default:
-        alert("Operacion erronea");
-        break;
+
+function calcular(operacion, primerNumero, segundoNumero) {//funcion para calcular
+  let resultado = 0;
+
+  switch (operacion) {
+    case '+':
+      resultado = primerNumero + segundoNumero;
+      break;
+    case '-':
+      resultado = primerNumero - segundoNumero;
+      break;
+    case '*':
+      resultado = primerNumero * segundoNumero;
+      break;
+    case '/':
+      resultado = primerNumero / segundoNumero;
+      break;
+    default:
+      alert("Operacion invalida");
+      break;
+  }
+
+  return resultado;
+}
+
+
+function ejecutarCalculadora() { //funcion para ejecutarla
+  const resultados = [];
+
+  while (true) {
+    const primerNumero = parseInt(prompt("Ingrese el primer numero:"));
+    const operacion = prompt("Ingrese que operacion desea realizar: (+, -, *, /):");
+    const segundoNumero = parseInt(prompt("Ingrese el segundo numero:"));
+
+    const resultado = calcular(operacion, primerNumero, segundoNumero);
+    resultados.push(resultado);
+
+    alert("El resultado es: " + resultado);
+
+    const repetir = prompt("¿Desea realizar otra operacion? (S/N)").toLowerCase();
+
+    if (repetir === 'n') {
+      break;
+    } else if (repetir !== 's') {
+      alert("Opcion invalida. Calculadora finalizada");
+      break;
     }
-    
-    return resultado;
   }
 
-  //funcion repetircalculadora
-  function repetirCalculadora(){
+  return resultados;
+}
 
-  //Le solicitamos los numeros al usuario
-  const primerNumero = parseInt(prompt("Ingrese el primer número:"));
-  const operacion = prompt("Ingrese la operacion que desea realizar: (+ , - , * , /):");
-  const segundoNumero = parseInt(prompt("Ingrese el segundo número:"));
-  
-  // calculamos el numero
-  const resultado = calcular(operacion, primerNumero, segundoNumero);
-  
-  // mostramos el resultado
-  alert("El resultado es: " + resultado);
+//Ejecutamos para obtener el resultado
+const resultadosCalculadora = ejecutarCalculadora();
 
-  // Le preguntamos al usuario si quiere continuar o salir
-  const repetir = prompt("¿Desea realizar otra operación? (S/N)").toLowerCase();
-
-  //If para repetir o finalizar
-  if (repetir === 's') {
-    repetirCalculadora();
-  } else if (repetir === 'n') {
-    alert("Calculadora finalizada.");
-  } else {
-    alert("Opcion invalida , Calculadora finalizada.");
-  }
-  }
-
-// repetir
-repetirCalculadora();
+//Usando map mostramos el resultado de todas las operaciones que ha realizado en la consola segun el orden al finalizarla
+console.log("El resultado es: ");
+resultadosCalculadora.map((resultado, indice) => {
+  console.log(`Operación ${indice + 1}: ${resultado}`);
+});
